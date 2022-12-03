953dce11cd59330b81ab0d5f7d336f57d5b7a0a6
Kazuto Kusama <jacopen@gmail.com>
2022-12-03 10:42:35 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "vsphere_virtual_machine" "centos9" {
+  name     = var.name
+
+  datastore_id     = var.datastore_id
+  resource_pool_id = var.resource_pool_id
+
+  num_cpus          = var.num_cpus
+  memory            = var.memory
+  guest_id          = "centos8_64Guest"
+  nested_hv_enabled = var.nested_hv_enabled
+  folder = var.folder
+  firmware = "efi"
+
+  network_interface {
+    network_id = var.network_id
+  }
+
+  disk {
+    label = "disk0"
+    size  = var.disk_size
+  }
+  cdrom {
+    client_device = true
+  }
+  clone {
+    template_uuid = var.template_uuid
+    linked_clone  = "false"
+  }
+  extra_config = {
+    "guestinfo.metadata" = base64encode(templatefile("${path.module}/templates/base/metadata.yaml", {
+      ip_address  = var.ip_address,
+      subnet_mask = var.subnet_mask,
+      gateway     = var.gateway,
+      nameservers = var.nameservers,
+      hostname    = var.hostname
+    }))
+    "guestinfo.metadata.encoding" = "base64"
+    "guestinfo.userdata"          = base64encode(file("${path.module}/templates/base/userdata.yaml"))
+    "guestinfo.userdata.encoding" = "base64"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "name" {
+  defualt = "CentOS"
+}
+
+variable "datastore_id" {
+
+}
+
+variable "resource_pool_id" {
+
+}
+
+variable "num_cpus" {
+  default = 2
+}
+
+variable "memory" {
+  default = 4096
+}
+
+variable "nested_hv_enabled" {
+  default = true
+}
+
+variable "folder" {
+  default = "Sandbox"
+}
+
+variable "network_id" {}
+
+variable "disk_size" {
+  default = 20
+}
+
+variable "template_uuid"  {
+
+}
+
+variable "ip_address" {
+  
+}
+
+variable "subnet_mask" {
+  
+}
+
+variable "gateway" {
+  
+}
+
+variable "nameservers" {
+  
+}
+
+variable "hostname" {
+  
+}
